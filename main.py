395e5fcf53d27e6aee4b9a029e638c6a924bdaa9
groozin <tomasz.mikos@gmail.com>
2024-11-01 11:33:53 +0100

    add circle shape and limnit fps to 60

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -16,6 +18,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60)
 
 if __name__ == '__main__':
     main()
